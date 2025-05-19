ea8311f5ea6daff85bc1d017da21a103822de818
Damian Icely <damian@yogalap.com>
2025-05-19 14:06:00 +0100

    revert

--- j/behaviour.js
+++ j/behaviour.js
@@ -1,50 +1,76 @@
+var black = (function () {
+
 $(function () {
-    // Smooth scroll links
-    $('#nav a, a.scroll').click(function () {
+    $('#nav a').add('a.scroll').click(function () {
         var target = $(this.hash);
+        var hash = this.hash;
+        // $target = $target.length && $target
+        // || $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
-            $('html, body').animate({ scrollTop: target.offset().top }, 600);
+            var targetOffset = target.offset().top;
+            $('html,body').animate({scrollTop: targetOffset}, 600);
             return false;
-        }
-    });
-
-    var $nav = $('#nav');
-    var $window = $(window);
-    var $container = $('#container');
-
-    var topmostPoint = $nav.offset().top;
-    var leftPoint = $container.offset().left;
-    var PADDING_TOP = 63;
-
-    function updateNavPosition() {
-        var scrollTop = $window.scrollTop();
-        if (scrollTop > (topmostPoint - PADDING_TOP)) {
-            $nav.css({
-                position: 'fixed',
-                top: PADDING_TOP,
-                left: $container.offset().left
-            });
+		}
+	});
+	
+	
+    // THIS IS REALLY FREAKING BIZARRE 
+    // I SHOULD NOT HAVE TO DO THIS BUT IE (6 & 7) ARE PRETTY MESSED UP.
+    $('#news').css({'position': 'absolute'});
+	var nav = $('#nav');
+	nav.css({'position': 'absolute'});
+	
+	/*
+	 * MAGICAL SCROLLING NAV
+	 * (not that magical)
+	 * 
+ 	 * This is using lots of vars because it gets called all the time 
+ 	 * on scroll, so it needs to be fast.
+	 */
+	var topmost_point = nav.offset().top;
+    var left_point = $('#container').offset().left;
+	var PADDING_TOP = 63; // MAGIC NUMBER
+    var REAL_TOP = topmost_point - PADDING_TOP;
+	var the_window = $(window);
+	var NAV_IS_FIXED = (nav.css('position') == 'fixed');
+	the_window.scroll(function () {
+        if (the_window.scrollTop() > REAL_TOP) {
+            if ($.browser.msie && $.browser.version == "6.0") {
+                nav.css('top', the_window.scrollTop() + PADDING_TOP);
+            } else if (!NAV_IS_FIXED) {
+        	    nav.css({
+                    left: left_point,
+        	        top: PADDING_TOP,
+        	        position: 'fixed'
+        	    });
+        	    NAV_IS_FIXED = true;
+        	}
         } else {
-            $nav.css({
-                position: 'absolute',
-                top: topmostPoint,
-                left: ''
-            });
+            if (NAV_IS_FIXED) {
+                nav.css({
+                    position: 'absolute',
+                    top: topmost_point,
+                    left: ''
+                });
+                NAV_IS_FIXED = false;
+            }
         }
-    }
-
-    $window.on('scroll', updateNavPosition);
-    $window.on('resize', function () {
-        leftPoint = $container.offset().left;
-        if ($nav.css('position') === 'fixed') {
-            $nav.css('left', leftPoint);
-        }
-    });
-
-    updateNavPosition(); // Initial call in case the page loads scrolled
-
-    // Optional hash navigation behavior
-    if (document.location.hash === '#order') {
-        show_order_form();
-    }
+	});
+	
+	the_window.resize(function () {
+	    left_point = $('#container').offset().left;
+	    if (NAV_IS_FIXED) {
+    	    nav.css('left', left_point);	        
+	    }
+	});
+	if (document.location.hash.match(/^#?order$/)) {
+	    show_order_form();
+	}	
 });
+
+//---
+    return {
+        format_price: format_price,
+        get_price: get_price
+    };
+})();
